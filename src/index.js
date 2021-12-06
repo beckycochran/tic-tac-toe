b52e6a380e1681c0f19273a51f5f7a53d07e67d5
Rebekah <rebekahmparkes@outlook.com>
2021-12-06 16:43:16 -0500

    test

--- src/index.js
+++ src/index.js
@@ -11,12 +11,13 @@ class Square extends React.Component {
       value: null,
     };
   }
-  
+
   render() {
     return (
-      <button className="square" onClick={() =>
-        console.log('click here')  }>
-        {this.props.value}
+      <button
+        className="square" 
+        onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
       </button>
     );
   }
@@ -24,7 +25,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square />;
   }
 
   render() {
